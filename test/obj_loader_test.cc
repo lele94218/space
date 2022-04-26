b10b9969b534f15a9252645a15397404355a75bb
lele94218 <terry.trxue@gmail.com>
2022-04-26 01:41:33 -0700

    setup test

--- test/obj_loader_test.cc
+++ test/obj_loader_test.cc
@@ -1,9 +1,16 @@
-#include <gtest/gtest.h>
-
 #include "../src/loaders/obj_loader.h"
 
+#include <glog/logging.h>
+#include <gtest/gtest.h>
+
 TEST(ObjLoaderTest, LoadLightBulb) {
-  ObjLoader loader("test");
-  EXPECT_STRNE("hello", "world");
-  EXPECT_EQ(7 * 6, 42);
+  ObjLoader loader("../assets/light_bulb/light_bulb.obj");
+  loader.Load();
+  LOG(ERROR) << loader.object_3d_->children().size();
+}
+
+int main(int argc, char** argv) {
+  google::InitGoogleLogging(argv[0]);
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
